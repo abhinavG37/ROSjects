a862f64b41929d8620dff0a1adc8693513452d94
Abhinav Gupta <abhinavgupta1998@gmail.com>
2021-07-27 15:44:06 +0530

    Model Spawn Successful
    With ROS Noetic macros to be called as xacro:<macro_name>
    mybot.gazebo had a couple of variables missing $ sign which caused a string to be passed to the macro and could not allow the compiler to evaluate a math expression

---
+++ ROSject_2/build/mybot_control/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/abhinav137/Desktop/ROSjects/ROSject_2/src/mybot_control
+# Build directory: /home/abhinav137/Desktop/ROSjects/ROSject_2/build/mybot_control
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ ROSject_2/build/mybot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/abhinav137/Desktop/ROSjects/ROSject_2/src/mybot_description
+# Build directory: /home/abhinav137/Desktop/ROSjects/ROSject_2/build/mybot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
